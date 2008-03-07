a3f430005b0887578b412aff3aa0b04e4d51e167
Duff OMelia <duff@omelia.org>
2008-03-07 17:46:05 -0500

    Basic signup

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,10 +1,8 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
+  helper :all
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'c0eade444ddb1314441b17efb4d3492c'
+  include AuthenticatedSystem
+
+  protect_from_forgery
 end
