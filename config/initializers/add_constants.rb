a113144bad9b2a484b9a3e1de2da748ca15c6a94
Duff OMelia <duff@omelia.org>
2008-03-07 17:52:43 -0500

    Oops.  Wrong app

--- config/initializers/add_constants.rb
+++ config/initializers/add_constants.rb
@@ -1,4 +1,4 @@
 APP_DOMAIN = RAILS_ENV == 'production' ? "thoughtlead.verticality.dock.terralien.biz" : "thought.dev"
 APP_PORT = RAILS_ENV == 'production' ? "" : ":3000"
-APP_NAME = "Spreedly"
+APP_NAME = "ThoughtLead"
 
