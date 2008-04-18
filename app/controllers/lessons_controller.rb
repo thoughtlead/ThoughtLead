824192064134153b19ed290a9440678ebb76626c
Duff OMelia <duff@omelia.org>
2008-04-18 15:43:01 -0400

    Can now insert images into lessons.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,7 +7,7 @@ class LessonsController < ApplicationController
   uses_tiny_mce(:options => 
 {    :mode => "specific_textareas",
   	:theme => "advanced",
-  	:theme_advanced_buttons1 => "bold,italic,|,bullist,numlist,outdent,indent,|,undo,redo",
+  	:theme_advanced_buttons1 => "bold,italic,|,bullist,numlist,outdent,indent,|,image,|,undo,redo",
   	:theme_advanced_buttons2 => "",
   	:theme_advanced_buttons3 => "",
   	:theme_advanced_toolbar_location => "top",
