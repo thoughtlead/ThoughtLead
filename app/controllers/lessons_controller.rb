553613d41b83d7140676b49e37322908d60528f2
Duff OMelia <duff@omelia.org>
2008-04-18 16:27:16 -0400

    Cleaned

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,17 +4,7 @@ class LessonsController < ApplicationController
   before_filter :owner_login_required, :except => [ :show ]
   before_filter :community_is_active
   
-  uses_tiny_mce(:options => 
-{    :mode => "specific_textareas",
-  	:theme => "advanced",
-  	:theme_advanced_buttons1 => "bold,italic,|,bullist,numlist,outdent,indent,|,image,|,undo,redo",
-  	:theme_advanced_buttons2 => "",
-  	:theme_advanced_buttons3 => "",
-  	:theme_advanced_toolbar_location => "top",
-  	:theme_advanced_toolbar_align => "left",
-    :extended_valid_elements => "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
-}
-  )
+  uses_tiny_mce(tiny_mce_options)
    
   def new
     @lesson = Lesson.new
@@ -52,5 +42,6 @@ class LessonsController < ApplicationController
       @course = current_community.courses.find(params[:course_id])
       @chapter = @course.chapters.find(params[:chapter_id])
     end
+    
   
 end
