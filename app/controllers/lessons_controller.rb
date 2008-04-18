150fc6db4f8d15fc3f16015b6e0072b93f894851
Duff OMelia <duff@omelia.org>
2008-04-18 11:12:07 -0400

    Experimenting using a rich text editor.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,6 +4,18 @@ class LessonsController < ApplicationController
   before_filter :owner_login_required, :except => [ :show ]
   before_filter :community_is_active
   
+  uses_tiny_mce(:options => 
+{    :mode => "textareas",
+  	:theme => "advanced",
+  	:theme_advanced_buttons1 => "bold,italic,|,bullist,numlist,outdent,indent,|,undo,redo",
+  	:theme_advanced_buttons2 => "",
+  	:theme_advanced_buttons3 => "",
+  	:theme_advanced_toolbar_location => "top",
+  	:theme_advanced_toolbar_align => "left",
+    :extended_valid_elements => "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
+}
+  )
+   
   def new
     @lesson = Lesson.new
   end
