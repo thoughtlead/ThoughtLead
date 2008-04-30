29d6f6a1fed7f37b8139049f9ae512b1368206b0
Daniel Beach <email@danielbeach.com>
2008-04-29 22:42:35 -0400

    Added avatar to left of discussion on discussions index.

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,6 +1,6 @@
 class Avatar < ActiveRecord::Base
   belongs_to      :user
-  has_attachment  :content_type => :image, :storage => :file_system, :thumbnails => { :small => '48x48>', :medium => '64x64>', :large => '128x128>' }, :max_size => 5.megabytes, :processor => (Rails.env == 'staging') ? 'MiniMagick' : 'ImageScience'
+  has_attachment  :content_type => :image, :storage => :file_system, :thumbnails => { :small => '48x48>', :medium => '64x64>', :large => '100x100>' }, :max_size => 5.megabytes, :processor => (Rails.env == 'staging') ? 'MiniMagick' : 'ImageScience'
   validates_as_attachment
 
 end
