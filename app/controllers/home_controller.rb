79e7c1dd18e26c2f34edead454fd086473e7d015
Duff OMelia <duff@omelia.org>
2008-05-06 10:11:09 -0400

    Added monitoring url so engine yard can monitor the site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   layout 'home'
+  session :off, :only => :status
   
   def index
     @communities = Community.find(:all)
+  end
+  
+  def status
+    render :text => "OK"
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.login '/login', :controller => "sessions", :action => 'new', :conditions => { :subdomain => /./ }
   map.logout '/logout', :controller => 'sessions', :action => 'destroy', :conditions => { :subdomain => /./ }
   map.signup '/signup', :controller => 'users', :action => 'signup', :conditions => { :subdomain => /./ }
+  map.status '/status', :controller => 'home', :action => 'status', :conditions => { :subdomain => '' }
 
   map.resources :sessions, :conditions => { :subdomain => /./ }
   map.resources :users, :member => { :email => :any, :edit_password => :any }, :conditions => { :subdomain => /./ }
