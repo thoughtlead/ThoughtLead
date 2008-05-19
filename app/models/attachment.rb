e13abac76af5875c234a706d9ac820728f8fce2a
Duff OMelia <duff@omelia.org>
2008-05-19 17:21:02 -0400

    Forgot to set attachment size back.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,7 @@
 class Attachment < ActiveRecord::Base
   belongs_to      :user
   belongs_to      :lesson
-  has_attachment  :storage => :s3, :max_size => 1.megabytes
+  has_attachment  :storage => :s3, :max_size => 25.megabytes
   validates_as_attachment
 
   def attachment_attributes_valid?
