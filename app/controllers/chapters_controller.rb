eadb8fcf7872cc5d071d44279bed302517f714ab
Duff OMelia <duff@omelia.org>
2008-05-23 13:08:07 -0400

    Owners can now delete chapters. Created an area where owners can edit chapters.  This will allow them to someday rename chapters as well.  This cleans up the interface some.

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -11,9 +11,22 @@ class ChaptersController < ApplicationController
     @course.chapters << @chapter
     
     flash[:notice] = "Successfully created"
-    redirect_to @course
+    redirect_to course_chapters_url(@course)
   end
 
-
+  def index
+    @course = current_community.courses.find(params[:course_id])
+    @chapters = @course.chapters
+  end
+  
+  def destroy
+    @course = current_community.courses.find(params[:course_id])
+    @chapter = @course.chapters.find(params[:id])
+    
+    @chapter.destroy
+    
+    flash[:notice] = "Successfully deleted the chapter named '#{@chapter}'"
+    redirect_to course_chapters_url(@course)
+  end
 
 end
