a36cf62c2a8eb03efbaca0179354bdcce6492422
Anthony Moquin <anthony.moquin@ternarysoftware.com>
2008-06-10 15:47:32 -0400

    Changed exception notifier to Ternary email address.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,3 +1,3 @@
-ExceptionNotifier.exception_recipients = %w(duff@omelia.org)
+ExceptionNotifier.exception_recipients = %w(autochewandroids@ternarysoftware.com)
 ExceptionNotifier.sender_address = "#{APP_NAME} Application Error <do-not-respond@#{APP_DOMAIN}>"
 ExceptionNotifier.email_prefix = "[#{APP_NAME} Error] "
