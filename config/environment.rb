f3d3fcfa4c57b7cfdd1fdeeeada0d5d5e939c999
Anthony Moquin <anthony.moquin@ternarysoftware.com>
2008-06-13 17:51:42 -0400

    freeze aws-s3 gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,11 @@ RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem "aws-s3", :lib => "aws/s3" 
+  config.gem "colored"
+  config.gem "map_by_method"
+  config.gem "RedCloth"
+  config.gem "redgreen"
 
   config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
     File.directory?(lib = "#{dir}/lib") ? lib : dir
