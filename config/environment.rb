c92e0a5c8996aba5d25ff993d38ffca8192d3a2b
Anthony Moquin <anthony.moquin@ternarysoftware.com>
2008-06-13 18:48:30 -0400

    redcloth needs to be required as all lowercase

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "RedCloth"
+  config.gem "RedCloth", :lib => "redcloth"
   config.gem "mime-types", :lib => "mime/types"
   config.gem "aws-s3", :lib => "aws/s3" 
   config.gem "colored"
