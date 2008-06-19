3728cc1fd9d8834a01cc3f88a9b1866e9dea68bc
Eric Diamond <egdiamond@gmail.com>
2008-06-19 17:40:32 -0400

    turns out filtered_remote_cache only works with svn

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :domain,          "gukumatz.tsi.local"
 set :rails_env,       "staging"
 set :deploy_to,       "/var/www/apps/#{application}"
 set :monit_group,     "verticality"
-set :deploy_via, :filtered_remote_cache
+set :deploy_via, :remote_cache
 
 role :web, domain
 role :app, domain
