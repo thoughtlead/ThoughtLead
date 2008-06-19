376ec2999a1dc8a81cbffd7003461146aa6eb18c
Anthony Moquin <anthony.moquin@ternarysoftware.com>
2008-06-19 14:44:57 -0400

    remove hack for staging environment

--- lib/subdomain_helper.rb
+++ lib/subdomain_helper.rb
@@ -1,8 +1,7 @@
 module SubdomainHelper
-
+  
   def subdomain_from(request)
-    return '' if (Rails.env == 'staging') && (request.subdomains == ["thoughtlead", "verticality", "dock"])
     request.subdomains.first.to_s
   end
-
+  
 end
