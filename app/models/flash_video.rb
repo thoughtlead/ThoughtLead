2d5ffb312a1d8e0e41b309cea29ab44964d60221
Alan Farmer <alan@ternarysoftware.com>
2008-07-07 16:10:02 -0400

    More feedback for uploading invalid attachments and flash files

--- app/models/flash_video.rb
+++ app/models/flash_video.rb
@@ -5,7 +5,7 @@ class FlashVideo < ActiveRecord::Base
   validates_as_attachment
 
   def attachment_attributes_valid?
-    errors.add_to_base("Uploaded file is too large (25MB max).") if attachment_options[:size] && !attachment_options[:size].include?(send(:size))
-    errors.add_to_base("Uploaded file has invalid content.") if attachment_options[:content_type] && !attachment_options[:content_type].include?(send(:content_type))
+    errors.add_to_base("Uploaded file #{filename} is too large (25MB max).") if attachment_options[:size] && !attachment_options[:size].include?(send(:size))
+    errors.add_to_base("Uploaded file #{filename} has invalid content.") if attachment_options[:content_type] && !attachment_options[:content_type].include?(send(:content_type))
   end 
 end
