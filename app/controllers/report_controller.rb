21dfe7ffa436cab49692ca48d81686db7cd983f9
Eric Diamond <egdiamond@gmail.com>
2008-08-11 16:47:50 -0400

    Modify user export to allow a date-since and choice of premium or registered users.

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -7,7 +7,9 @@ class ReportController < ApplicationController
   # example action to return the contents
   # of a table in CSV format
   def export_users
-    users = User.find(:all)
+    since = Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i)
+    active = params[:premium] == 'true'
+    users = User.find(:all, :conditions => ["created_at >= ? and community_id = ? and active = ?", since, current_community.id, active])
     csv_string = FasterCSV.generate do |csv| 
       csv << ["first_name","last_name","email"]
       users.each do |u|
@@ -17,6 +19,7 @@ class ReportController < ApplicationController
     send_data csv_string, :type => "text/plain", 
     :filename=>"export_users.csv", 
     :disposition => 'attachment'
+    
   end
   
 end
