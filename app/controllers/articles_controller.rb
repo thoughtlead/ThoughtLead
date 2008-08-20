818ec8d1ebc57bbbe6ed4fa26f16f7c569e630e0
Alan Farmer <alan@ternarysoftware.com>
2008-08-20 10:25:58 -0400

    Sort library articles by modification date

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   
   def index
     @articles = current_community.articles.for_category(params[:category])
-    @articles = @articles.sort_by{ |article| article.created_at }
+    @articles = @articles.sort_by{ |article| article.updated_at }
     @articles = @articles.reverse
     @category = current_community.categories.find_by_id(params[:category]) if params[:category] && params[:category] != 'nil'
   end
