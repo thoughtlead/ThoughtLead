2b4b08b8d275a8a1e1ab22b1022c2fd9fd8fdc38
thame <tomfadial@gmail.com>
2008-09-01 12:47:10 -0400

    Removed link to theme image from IE stylesheet.

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -13,9 +13,18 @@ jQuery(function($) {
 		applyCorners : function() {
 			thoughtlead.markupCorners($e.main_content.find('.banner'));
 		},
+		screenshotPNG : function() {
+			var image = $('.banner .image');
+			var src = image.find('img').attr('src');
+
+			if ($.browser.msie && parseInt($.browser.version) == 6) {
+				image.css('filter','progid:DXImageTransform.Microsoft.AlphaImageLoader(src="'+src+'", sizingMethod="image");')
+			}
+		},
 		init : function() {
 			home.listHooks();
 			home.applyCorners();
+			home.screenshotPNG();
 		}
 	};
 	home.init();
