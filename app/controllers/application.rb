6109edd27b1e1ff2591dda9c6258053cec800a45
Eric Diamond <egdiamond@gmail.com>
2008-09-09 09:13:57 -0400

    enable creating links in TinyMCE

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -53,7 +53,7 @@ class ApplicationController < ActionController::Base
       {   
         :mode => "specific_textareas",
         :theme => "advanced",
-        :theme_advanced_buttons1 => "bold,italic,|,bullist,numlist,outdent,indent,|,image,|,undo,redo",
+        :theme_advanced_buttons1 => "bold,italic,|,bullist,numlist,outdent,indent,|,link,image,|,undo,redo",
         :theme_advanced_buttons2 => "",
         :theme_advanced_buttons3 => "",
         :theme_advanced_toolbar_location => "top",
