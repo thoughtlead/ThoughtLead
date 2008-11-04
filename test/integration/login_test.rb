b5ad3acb8dd970cc78791b88eb9808c81d718b3b
Alan Farmer <alan@ternarysoftware.com>
2008-11-04 14:49:11 -0500

    Add a test to confirm the right person's password is reset when there are same-login folks in different communities.

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -21,6 +21,18 @@ class LoginTest < ActionController::IntegrationTest
     alex_session.assert_logged_in(:alex)
   end
   
+  def test_forgot_password_resets_correct_password
+    donny1 = users :donnydupe1
+    donny2 = users :donnydupe2
+    donny1_password_before = donny1.crypted_password
+    donny2_password_before = donny2.crypted_password
+    post "http://c2.nokahuna.dev/forgot_password", :login => donny1.login
+    donny1.reload
+    donny2.reload
+    assert_equal(donny1_password_before, donny1.crypted_password, "The wrong donny's password changed.")
+    assert_not_equal(donny2_password_before, donny2.crypted_password, "The right donny's password didn't change.")
+  end
+  
   def test_unable_to_login_to_another_site
     new_session('c1.nokahuna.dev') do
       assert(login(:duff))
