77baafc181f345c7422e6f6c294f58ddc3e2a8c4
Alan Farmer <alan@ternarysoftware.com>
2008-12-16 10:26:10 -0500

    update various app files to rails 2.2.2

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,14 +2,9 @@
 
 # Add new inflection rules using the following format 
 # (all these examples are active by default):
-# Inflector.inflections do |inflect|
+# ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
-
-#Inflector.inflections do |inflect| 
-#  inflect.plural /(m)edia$/i, '\1edia'
-#  inflect.singular /(m)edia$/i, '\1edia'
-#end
