b14b0196a88ddddfe688efd3c5adf97cf600c53e
Alan Farmer <alan@ternarysoftware.com>
2009-01-05 14:56:56 -0500

    Filter out disabled users from member list before paginating unless the current user is the community admin.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
   
   def index
     @users = current_community.users
+    @users = @users.find_all { |user| !user.disabled? } unless logged_in_as_owner?
     @users = @users.paginate :page => params[:page], :per_page => 15
   end
   
