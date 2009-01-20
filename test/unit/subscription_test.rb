5800f5627ea2f648375f1ae8454d34f655647eaa
Scott Hunter <shunter@ternarysoftware.com>
2009-01-20 14:28:22 -0500

    switch to shoulda style

--- test/unit/subscription_test.rb
+++ test/unit/subscription_test.rb
@@ -1,43 +1,56 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class SubscriptionTest < ActiveSupport::TestCase
-  def test_renewal_summary
-    subscription = subscriptions :premium
+  context "A Subscription" do
+    setup do
+      @subscription = Subscription.new
+      @subscription.amount = 10.00
+      @subscription.renewal_period = 2
+      @subscription.renewal_units = "months"
+    end
 
-    subscription.renewal_period = 1
-    subscription.renewal_units = "months"
+    should "have a renewal summary that includes the correct units" do
+      assert_equal("2 months", @subscription.renewal_summary)
 
-    assert_equal("month", subscription.renewal_summary)
+      @subscription.renewal_units = "days"
+      assert_equal("2 days", @subscription.renewal_summary)
 
-    subscription.renewal_units = "days"
-    assert_equal("day", subscription.renewal_summary)
+      @subscription.renewal_units = "weeks"
+      assert_equal("2 weeks", @subscription.renewal_summary)
 
-    subscription.renewal_units = "weeks"
-    assert_equal("week", subscription.renewal_summary)
+      @subscription.renewal_units = "years"
+      assert_equal("2 years", @subscription.renewal_summary)
+    end
 
-    subscription.renewal_units = "years"
-    assert_equal("year", subscription.renewal_summary)
+    should "not include the number and be singular in renewal summary when renewal period is only 1" do
+      @subscription.renewal_period = 1
+      assert_equal("month", @subscription.renewal_summary)
 
-    subscription.renewal_period = 6
-    assert_equal("6 years", subscription.renewal_summary)
+      @subscription.renewal_units = "days"
+      assert_equal("day", @subscription.renewal_summary)
 
-    subscription.renewal_units = "months"
-    assert_equal("6 months", subscription.renewal_summary)
-  end
+      @subscription.renewal_units = "weeks"
+      assert_equal("week", @subscription.renewal_summary)
 
-  def test_summary
-    subscription = subscriptions :premium
+      @subscription.renewal_units = "years"
+      assert_equal("year", @subscription.renewal_summary)
+    end
 
-    subscription.amount = 10.00
-    subscription.renewal_period = 1
-    subscription.renewal_units = "weeks"
+    should "include the amount in the summary" do
+      assert_equal("$10.00 per 2 months", @subscription.summary)
+    end
 
-    assert_equal("$10.00 per week", subscription.summary)
+    should "not include the number and be singular in summary when renewal period is only 1" do
+      @subscription.renewal_period = 1
+      assert_equal("$10.00 per month", @subscription.summary)
+    end
 
-    subscription.renewal_units = "months"
-    assert_equal("$10.00 per month", subscription.summary)
+    should "display correct unit in summary" do
+      @subscription.renewal_units = "days"
+      assert_equal("$10.00 per 2 days", @subscription.summary)
 
-    subscription.renewal_period = 6
-    assert_equal("$10.00 per 6 months", subscription.summary)
+      @subscription.renewal_units = "weeks"
+      assert_equal("$10.00 per 2 weeks", @subscription.summary)
+    end
   end
 end
