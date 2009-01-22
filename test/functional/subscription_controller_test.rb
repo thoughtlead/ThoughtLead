802e9e1f5af66ca52f1af777f47f11c32075928c
Audrey <audrey@ternarysoftware.com>
2009-01-22 15:32:41 -0500

    Test that an owner cannot enter biling information or get charged for a subscription change---the owner has free access to everything. Also, fix test that setting subscription to "free" actually removes your access class and also fix the code (both were a little broken)

--- test/functional/subscription_controller_test.rb
+++ test/functional/subscription_controller_test.rb
@@ -20,39 +20,59 @@ class SubscriptionControllerTest < ActionController::TestCase
   end
 
   context "on UPDATE subscription to a paid subscription" do
-    setup do
-      plan = subscription_plans :c1_premium_monthly
-      community = communities :c1
-      sam = users :sam
-      new_request(community, sam)
-      post :update, {:subscription => {:subscription_plan_id => plan.id}}
+    context "when the user is not the community owner" do
+      setup do
+        plan = subscription_plans :c1_premium_monthly
+        community = communities :c1
+        sam = users :sam
+        new_request(community, sam)
+        put :update, {:subscription => {:subscription_plan_id => plan.id}}
+      end
+
+      should_respond_with :redirect
+
+      should "actually set the plan" do
+        plan = subscription_plans :c1_premium_monthly
+        sam = users :sam
+        assert_equal plan, sam.subscription.subscription_plan
+      end
     end
 
-    should_respond_with :redirect
+    context "when the user is the community owner" do
+      setup do
+        plan = subscription_plans :c3_premium_monthly
+        community = communities :c3
+        user = users :c3_community_owner
+        new_request(community, user)
+        put :update, {:subscription => {:subscription_plan_id => plan.id}}
+      end
 
-    should "actually set the plan" do
-      plan = subscription_plans :c1_premium_monthly
-      sam = users :sam
-      assert_equal plan, sam.subscription.subscription_plan
+      should_redirect_to "user_url(@subscription.user.id)"
+
+      should "actually set the plan" do
+        plan = subscription_plans :c3_premium_monthly
+        user = users :c3_community_owner
+        assert_equal plan, user.subscription.subscription_plan
+      end
     end
   end
 
   context "on UPDATE subscription to free" do
     setup do
       audrey = User.find(users(:premium_audrey).id)
-
       assert_not_nil audrey.access_class
-
+      assert_not_nil audrey.subscription
       community = communities :c3
       new_request(community, audrey)
-      post :update, {:subscription => {:subscription_plan_id => 'Free'}}
+      put :update, {:subscription => {:subscription_plan_id => 'free'}}
     end
 
     should_respond_with :redirect
 
     should "actually set the user's access class to nil" do
       audrey = User.find(users(:premium_audrey).id)
-      assert audrey.access_class.nil?, "user should not have an access class if they choose the free plan"
+      assert_nil audrey.subscription, "User should not have a subscription"
+      assert_nil audrey.access_class, "user should not have an access class if they choose the free plan"
     end
   end
 end
