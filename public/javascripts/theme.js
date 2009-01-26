9a33ade8de1a7cb0ce474a5f58fdca638bc9c8e1
Audrey <audrey@ternarysoftware.com>
2009-01-26 09:21:13 -0500

    The owner can edit the accessibility for themes. Also, themes in the index indicate their accessibility so that it is easy to see at a glance what access classes they have.

---
+++ public/javascripts/theme.js
@@ -0,0 +1,21 @@
+registeredFlagChanged = function() {
+  var enable = true;
+  if ($('theme_public').checked) {
+    enable = false;
+  }
+  $$('.theme_access_class').each(function(element) {
+    if (enable) {
+      element.enable();
+    }
+    else {
+     element.checked = false;
+     element.disable();
+    }
+  });
+};
+
+Event.observe(window, 'load', function() {
+  $('theme_public').observe('change', registeredFlagChanged);
+  $('theme_registered').observe('change', registeredFlagChanged);
+  registeredFlagChanged();
+});
