3a2e00a0050148d063c1824eb8c77863a3951dfe
Audrey <audrey@ternarysoftware.com>
2009-01-27 09:40:11 -0500

    Tweak format of data on payment history table

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,6 +1,8 @@
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :month_day => "%b %d",
   :day_month_year => "%d %b %y",
-  :month_year => "%m.%Y"
-  
+  :month_year => "%m.%Y",
+  :date_time  => "%m/%d/%Y %I:%M%p"
+
+
 )
