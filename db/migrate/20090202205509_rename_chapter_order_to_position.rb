4cf173cfc18132169582b208b4673873a57df8c5
Scott Hunter <shunter@ternarysoftware.com>
2009-02-02 16:37:44 -0500

    apparently the schema that results from the migrations doesn't have an order field on chapter.  I don't know why it was present in my development DB, but in any case, we need to add the position column instead of renaming order.

--- db/migrate/20090202205509_rename_chapter_order_to_position.rb
+++
@@ -1,9 +0,0 @@
-class RenameChapterOrderToPosition < ActiveRecord::Migration
-  def self.up
-    rename_column :chapters, :order, :position
-  end
-
-  def self.down
-    rename_column :chapters, :position, :order
-  end
-end
