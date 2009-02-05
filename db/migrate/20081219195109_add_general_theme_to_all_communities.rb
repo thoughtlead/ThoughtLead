a25afc3cc0d0dc97c857a608f7a0f34f11c7f2b4
Audrey <audrey@ternarysoftware.com>
2009-02-05 11:18:05 -0500

    use models local to the migration instead of the real models

--- db/migrate/20081219195109_add_general_theme_to_all_communities.rb
+++ db/migrate/20081219195109_add_general_theme_to_all_communities.rb
@@ -1,4 +1,17 @@
 class AddGeneralThemeToAllCommunities < ActiveRecord::Migration
+  class Community < ActiveRecord::Base
+    has_many :themes, :class_name => "AddGeneralThemeToAllCommunities::Theme"
+    has_many :discussions, :class_name => "AddGeneralThemeToAllCommunities::Discussion"
+  end
+
+  class Theme < ActiveRecord::Base
+    belongs_to :community, :class_name => "AddGeneralThemeToAllCommunities::Community"
+  end
+
+  class Discussion < ActiveRecord::Base
+    belongs_to :theme, :class_name => "AddGeneralThemeToAllCommunities::Theme"
+  end
+
   def self.up
   	Community.find(:all).each do |community|
     	theme = Theme.new
@@ -6,15 +19,15 @@ class AddGeneralThemeToAllCommunities < ActiveRecord::Migration
     	theme.name = "General"
     	theme.description = "Ask questions, contribute your expertise, and engage with other members. Sort discussions by topic using the tabs to the right."
     	theme.save
-    	
+
     	community.discussions.each do |discussion|
-			if discussion.theme.nil?
-				discussion.theme_id = theme.id
-				discussion.save
-			end
+  			if discussion.theme.nil?
+  				discussion.theme_id = theme.id
+  				discussion.save
+  			end
     	end
   	end
-  		
+
   end
 
   def self.down
@@ -22,5 +35,3 @@ class AddGeneralThemeToAllCommunities < ActiveRecord::Migration
   	raise ActiveRecord::IrreversibleMigration
   end
 end
-
-
