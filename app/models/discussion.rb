dac27d23f030ea5cb5f794386a67bcaef465d2fd
Jacob Stetser <jake@wideopenspac.es>
2009-08-18 23:15:20 -0600

    Start using the database optimisations I just committed

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,15 +1,18 @@
 class Discussion < ActiveRecord::Base
   belongs_to :user
   belongs_to :community
-  belongs_to :theme
+  belongs_to :theme, :counter_cache => true
   has_many :responses, :dependent => :destroy
   has_many :email_subscriptions, :dependent => :destroy
   has_many :subscribers, :class_name => 'User', :through => :email_subscriptions
 
   named_scope :get_all, :include => [:theme, :responses]
+  named_scope :by_age, :include => [:theme, :responses], :order => 'thread_last_updated_at DESC'
   named_scope :uncategorized, :conditions => { :theme_id => nil }
 
   validates_presence_of :title, :body, :theme
+  
+  before_save :set_thread_timestamp
 
   is_indexed :fields => ['title', 'body', 'community_id']
 
@@ -31,5 +34,11 @@ class Discussion < ActiveRecord::Base
   def is_visible_to(user)
     return true if theme.nil?
     return theme.is_visible_to(user)
+  end
+  
+  protected 
+  
+  def set_thread_timestamp
+    thread_last_updated_at = (created_at || Time.now) if thread_last_updated_at.nil?
   end
 end
