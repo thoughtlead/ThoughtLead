6ae13e43c37b7c7811d0a565856a03465ccecd0e
Jacob Stetser <jake@wideopenspac.es>
2009-08-20 13:15:53 -0600

    Scopes for accessing affiliate data by month

--- app/models/affiliate_action.rb
+++ app/models/affiliate_action.rb
@@ -1,4 +1,12 @@
 class AffiliateAction < ActiveRecord::Base
   belongs_to :referrer, :class_name => 'User', :foreign_key => 'referrer_id'
   belongs_to :referred, :class_name => 'User', :foreign_key => 'referred_id'
+  
+  named_scope :this_month, :conditions => { :created_at => Time.now.utc.beginning_of_month..Time.now.utc }
+  named_scope :last_month, :conditions => { 
+    :created_at => (Time.now.utc - 1.month).beginning_of_month..(Time.now.utc - 1.month).end_of_month }
+  named_scope :months_ago, lambda { |months_ago|
+    t = Time.now.utc - months_ago.months
+    { :conditions => { :created_at => t.beginning_of_month..t.end_of_month } }
+  }
 end
