448aa1809f116f5385c47a5983ae9c800782d0c2
Jacob Stetser <jake@wideopenspac.es>
2010-01-04 05:39:30 -0700

    Re-enabled MCE editor on pages screens, and add button for editing HTML view FTW

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -119,6 +119,24 @@ class ApplicationController < ActionController::Base
       }
     }
   end
+  
+  def self.tiny_mce_options_with_code_toggle
+    {
+      :options =>
+      {
+        :mode => "specific_textareas",
+        :theme => "advanced",
+        :plugins => %w{ paste autoresize },
+        :paste_auto_cleanup_on_paste => "true",
+        :theme_advanced_buttons1 => "bold,italic,|,bullist,numlist,outdent,indent,|,link,image,|,pastetext,pasteword,code,|,undo,redo",
+        :theme_advanced_buttons2 => "",
+        :theme_advanced_buttons3 => "",
+        :theme_advanced_toolbar_location => "top",
+        :theme_advanced_toolbar_align => "left",
+        :extended_valid_elements => "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
+      }
+    }
+  end
 
   def themed_file(filename)
     themes_dir = File.expand_path(File.dirname(__FILE__) + "/../../public/themes")
