bc9e28cb7b25aeb188c801a43803706f68228f99
Jacob Stetser <jake@wideopenspac.es>
2010-01-21 12:03:51 -0700

    Send errors to new error email address: thoughtlead_errors@wideopenspac.es

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,3 +1,3 @@
-ExceptionNotifier.exception_recipients = %w(thoughtleaddevelopers@ternarysoftware.com)
+ExceptionNotifier.exception_recipients = %w(thoughtlead_errors@wideopenspac.es)
 ExceptionNotifier.sender_address = "#{APP_NAME} Application Error <do-not-respond@#{APP_DOMAIN}>"
 ExceptionNotifier.email_prefix = "[#{APP_NAME} Error] "
