4d3794bd96cfeb71159f7d0028a8aab5c364def2
Mikel Lindsaar <mikel@rubyx.com>
2010-09-10 13:17:50 +1000

    Adding links for sphinx conf

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,4 +1,6 @@
-links = { 'avatars' => 'public/avatars' }
+# Links are in the format "source file" => "Link"
+links = { 'avatars' => 'public/avatars',
+          'config/ultrasphinx/production.conf', 'config/sphinx_production.conf' }
 
 links.each_pair do |shared_folder,app_folder|
   run "echo 'linking: #{shared_path}/#{shared_folder} to #{release_path}/#{app_folder}' >> #{shared_path}/logs.log"
