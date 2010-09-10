74b08933d2e8b108069430d2dadd5d13d326bd34
Mikel Lindsaar <mikel@rubyx.com>
2010-09-10 13:31:50 +1000

    Final fix on ultrasphinx

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,6 +1,6 @@
 # Links are in the format "share path" => "release path"
 links = { 'avatars' => 'public/avatars',
-          'config/sphinx_production.conf' => 'config/ultrasphinx/production.conf' }
+          'config/ultrasphinx/production.conf', 'config/ultrasphinx/production.conf' }
 
 links.each_pair do |shared_folder, app_folder|
   run "echo 'linking: #{shared_path}/#{shared_folder} to #{release_path}/#{app_folder}' >> #{shared_path}/logs.log"
